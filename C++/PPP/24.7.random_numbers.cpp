755d0e8443343233ea206cd203a077aba129e78e
Genius320 <1412167533@qq.com>
2017-08-31 13:18:06 +0800

    Learning from PPP

--- C++/PPP/24.7.random_numbers.cpp
+++ C++/PPP/24.7.random_numbers.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #include <random>
 #include <vector>
 #include <functional>
@@ -10,3 +11,22 @@ int rand(int min, int max)
 }
 
 auto gen = bind(normal_distribution<double>{15, 4.0}, default_random_engine {});   // 均值，方差
+
+void f()
+{
+    vector<int> hist(2*15);
+    for (int i = 0; i < 500; ++i)
+        ++hist[int(round(gen()))];
+    for (int i = 0; i < hist.size(); ++i) {
+        cout << i << '\t';
+        for (int j = 0; j < hist[i]; ++j)
+            cout << '*';
+        cout << '\n';
+    }
+}
+
+int main()
+{
+    f();
+    return 0;
+}
