214c834f90f001b7d8d65ce0f523da25654e1991
LiuChenxuan <1412167533@qq.com>
2018-02-19 18:54:32 +0800

    Functions

---
+++ C/TCPL/Chapter01/exercise_1-15.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+float celsius(float fahr);
+
+int main()
+{
+    for (int fahr = 0; fahr <= 300; fahr += 20)
+        printf("%3d %6.1f\n", fahr, celsius(fahr));
+}
+
+/* convert fahrenheit into celsius */
+float celsius(float fahr)
+{
+	return (5.0 / 9.0) * (fahr - 32);
+}
