9282f88e1030f6e5d3fec406ff8b35d14252bc39
LiuChenxuan <1412167533@qq.com>
2018-02-20 21:29:46 +0800

    Character Arrays

--- C/TCPL/Chapter01/exercise_1-17.c
+++ C/TCPL/Chapter01/exercise_1-17.c
@@ -1,3 +1,8 @@
+/*
+	Date: 2018.2.19
+	Author: 刘臣轩
+*/
+
 #include <stdio.h>
 #define MAXLINE		1000
 #define LONGLINE	80
--- C/TCPL/Chapter01/exercise_1-18.c
+++ C/TCPL/Chapter01/exercise_1-18.c
@@ -1,3 +1,8 @@
+/*
+	Date: 2018.2.19
+	Author: 刘臣轩
+*/
+
 #include <stdio.h>
 #include <string.h>
 #define MAXLINE	1000
