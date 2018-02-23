b4b5299850d3627cd2f44c7b3ed1c1f148111295
LiuChenxuan <1412167533@qq.com>
2018-02-23 18:37:46 +0800

    Convert string s to double.

--- C/TCPL/Chapter01/exercise_1-12.c
+++ C/TCPL/Chapter01/exercise_1-12.c
@@ -6,20 +6,20 @@
 #include <stdio.h>
 
 #define IN	1
-#define OUT 0
+#define OUT	0
 
 int main()
 {
-	int c;
-	int state = OUT;
-	while ((c = getchar()) != EOF) {
-		if ((c == ' ' || c == '\t' || c == '\n') && state == IN) {
-			state = OUT;
-			putchar('\n');
-		} else if (state == OUT) {
-			state = IN;
-			putchar(c);
-		} else
-			putchar(c);
-	}
+    int c;
+    int state = OUT;
+    while ((c = getchar()) != EOF) {
+        if ((c == ' ' || c == '\t' || c == '\n') && state == IN) {
+            state = OUT;
+            putchar('\n');
+        } else if (state == OUT) {
+            state = IN;
+            putchar(c);
+        } else
+            putchar(c);
+    }
 }
---
+++ C/TCPL/Chapter04/exercise_4-2.c
@@ -0,0 +1,55 @@
+/*
+	Date: 2018.2.23
+	Author: 刘臣轩
+*/
+
+#include <ctype.h>
+#include <math.h>
+#include <stdio.h>
+
+double atof(char s[]);
+int atoi(char s[]);
+
+int main()
+{
+    printf("atof(\"123\") = %g\n", atof("123"));
+    printf("atof(\"-123\") = %g\n", atof("-123"));
+    printf("atof(\"123.45\") = %g\n", atof("123.45"));
+    printf("atof(\"-123.45\") = %g\n", atof("-123.45"));
+    printf("atof(\"123.45e6\") = %g\n", atof("123.45e6"));
+    printf("atof(\"-123.45e6\") = %g\n", atof("-123.45e6"));
+    printf("atof(\"123.45e-6\") = %g\n", atof("123.45e-6"));
+    printf("atof(\"-123.45e-6\") = %g\n", atof("-123.45e-6"));
+    return 0;
+}
+
+/* convert string s to double */
+double atof(char s[])
+{
+    int i;
+    for (i = 0; isspace(s[i]); ++i)
+        ;
+    int sign = (s[i] == '-') ? -1 : 1;
+    if (s[i] == '+' || s[i] == '-')
+        ++i;
+    double val;
+    for (val = 0.0; isdigit(s[i]); ++i)
+        val = 10.0 * val + (s[i] - '0');
+    if (s[i] == '.')
+        ++i;
+    double power;
+    for (power = 1.0; isdigit(s[i]); ++i) {
+        val = 10.0 * val + (s[i] - '0');
+        power *= 10.0;
+    }
+    val = sign * val / power;
+    int p = 0;
+    if (s[i] == 'e' || s[i] == 'E')
+        p = atoi(&s[++i]);
+    return val * pow(10, p);
+}
+
+int atoi(char s[])
+{
+    return (int)atof(s);
+}
