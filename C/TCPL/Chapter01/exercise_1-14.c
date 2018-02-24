b6b792132d4ca537077487873879f40a20d26e9d
LiuChenxuan <1412167533@qq.com>
2018-02-24 13:13:39 +0800

    Arrays

--- C/TCPL/Chapter01/exercise_1-14.c
+++ C/TCPL/Chapter01/exercise_1-14.c
@@ -3,39 +3,39 @@
 	Author: 刘臣轩
 */
 
-#include <stdio.h>
 #include <ctype.h>
+#include <stdio.h>
 
-#define MAXHIST	15
-#define MAXCHAR	128
+#define MAXHIST 15
+#define MAXCHAR 128
 
 int main()
 {
-	int nc[MAXCHAR] = { 0 };	// numbers of chars
+    int nc[MAXCHAR] = { 0 }; // numbers of chars
 
-	int c;
-	while ((c = getchar()) != EOF) {
-		if (c < MAXCHAR)
-			++nc[c];
-	}
+    int c;
+    while ((c = getchar()) != EOF) {
+        if (c < MAXCHAR)
+            ++nc[c];
+    }
 
-	int maxvalue = 0;
-	for (int i = 1; i < MAXCHAR; ++i)
-		if (nc[i] > maxvalue)
-			maxvalue = nc[i];
+    int maxvalue = 0;
+    for (int i = 1; i < MAXCHAR; ++i)
+        if (nc[i] > maxvalue)
+            maxvalue = nc[i];
 
-	for (int i = 1; i < MAXCHAR; ++i) {
-		if (isprint(i))
-			printf("%4d - %c - %3d : ", i, i, nc[i]);
-		else
-			printf("%4d -   - %3d : ", i, nc[i]);
-		int len = 0;
-		if (nc[i] > 0) {
-			if ((len = nc[i] * MAXHIST / maxvalue) == 0)
-				len = 1;
-		}
-		for (int j = len; j > 0; --j)
-			putchar('*');
-		putchar('\n');
-	}
+    for (int i = 1; i < MAXCHAR; ++i) {
+        if (isprint(i))
+            printf("%4d - %c - %3d : ", i, i, nc[i]);
+        else
+            printf("%4d -   - %3d : ", i, nc[i]);
+        int len = 0;
+        if (nc[i] > 0) {
+            if ((len = nc[i] * MAXHIST / maxvalue) == 0)
+                len = 1;
+        }
+        for (int j = len; j > 0; --j)
+            putchar('*');
+        putchar('\n');
+    }
 }
