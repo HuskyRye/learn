aa29400e65816dd1ef9465d54ed173e2b3226b8b
LiuChenxuan <1412167533@qq.com>
2018-03-01 18:20:59 +0800

    entend entab and detab to accept -m +n

---
+++ C/TCPL/Chapter05/exercise_5-12.c
@@ -0,0 +1,121 @@
+/*
+	Date: 2018.3.1
+	Author: 刘臣轩
+*/
+
+#include <stdbool.h>
+#include <stdio.h>
+
+#define TABINC 8
+#define MAXLINE 100
+
+void settab(int argc, char* argv[], bool* tab);
+void detab(bool* tab);
+void entab(bool* tab);
+bool tabpos(int pos, bool* tab);
+
+int main(int argc, char* argv[])
+{
+    bool tab[MAXLINE + 1];
+    settab(argc, argv, tab);
+    detab(tab);
+    entab(tab);
+    return 0;
+}
+
+#include <stdlib.h>
+
+void settab(int argc, char* argv[], bool* tab)
+{
+    int pos;
+    int inc;
+    if (argc <= 1) {
+        for (int i = 0; i <= MAXLINE; ++i) {
+            if (i % TABINC == 0)
+                tab[i] = true;
+            else
+                tab[i] = false;
+        }
+    } else if (argc == 3 && *argv[1] == '-' && *argv[2] == '+') {
+        pos = atoi(&(*(++argv))[1]);
+        inc = atoi(&(*(++argv))[1]);
+        for (int i = 0; i <= MAXLINE; ++i) {
+            if (i != pos)
+                tab[i] = false;
+            else {
+                tab[i] = true;
+                pos += inc;
+            }
+        }
+    } else {
+        for (int i = 0; i <= MAXLINE; ++i)
+            tab[i] = false;
+        while (--argc > 0) {
+            pos = atoi(*(++argv));
+            if (pos > 0 && pos <= MAXLINE)
+                tab[pos] = true;
+        }
+    }
+}
+
+bool tabpos(int pos, bool* tab)
+{
+    if (pos > MAXLINE)
+        return true;
+    else
+        return tab[pos];
+}
+
+void detab(bool* tab)
+{
+    int c;
+    int pos = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == '\t') {
+            do {
+                putchar(' ');
+                ++pos;
+            } while (!tabpos(pos, tab));
+        } else if (c == '\n') {
+            putchar(c);
+            pos = 0;
+        } else {
+            putchar(c);
+            ++pos;
+        }
+    }
+}
+
+void entab(bool* tab)
+{
+    int c;
+    int pos = 0;
+    int nb = 0;
+    int nt = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == ' ') {
+            ++pos;
+            if (!tabpos(pos, tab))
+                ++nb;
+            else {
+                nb = 0;
+                ++nt;
+            }
+        } else {
+            for (; nt > 0; --nt)
+                putchar('\t');
+            if (c == '\t')
+                nb = 0;
+            for (; nb > 0; --nb)
+                putchar(' ');
+            putchar(c);
+            if (c == '\n')
+                pos = 0;
+            else if (c == '\t')
+                while (!tabpos(pos, tab))
+                    ++pos;
+            else
+                ++pos;
+        }
+    }
+}
