8ae39e421f8be3d238ba1792a989880af7cb89bc
LiuChenxuan <1412167533@qq.com>
2018-03-23 21:38:12 +0800

    Learning from ice1000

--- C++/cell.cpp
+++ C++/cell.cpp
@@ -1,10 +1,9 @@
+#include <ctime> // time
+#include <iomanip> // setw
 #include <iostream>
-#include <iomanip>      // setw
-#include <string.h>		// memset
-#include <ctime>		// time
-#include <stdlib.h>     // rand
-#include <windows.h>    // sleep
-
+#include <stdlib.h> // rand
+#include <string.h> // memset
+#include <windows.h> // sleep
 
 using namespace std;
 
@@ -12,7 +11,7 @@ bool cells[20][20];
 
 void help();
 void print();
-void develop();			// this is really fun!
+void develop(); // this is really fun!
 
 int main()
 {
@@ -23,7 +22,6 @@ int main()
     cout << "you have 20x20 cells here\n";
     help();
     cout << "what's next? ";
-
 
     srand(time(NULL));
 
@@ -40,7 +38,7 @@ int main()
                 cerr << "x or y out of range!\n";
                 break;
             }
-            cells[y-1][x-1] = true;
+            cells[y - 1][x - 1] = true;
             break;
         case 'q':
             cells[1][2] = true;
@@ -59,26 +57,25 @@ int main()
             print();
             break;
         case 's':
-            cout << "ϸ���Ƿ�����ȡ������Χ8�����е�ϸ������\n";
-            cout << "��ǰϸ��Ϊ���״̬ʱ������Χ����2����������2�������ϸ��ʱ�� ��ϸ���������״̬\n";
-            cout << "��ǰϸ��Ϊ���״̬ʱ������Χ��2����3�����ϸ��ʱ�� ��ϸ������ԭ��\n";
-            cout << "��ǰϸ��Ϊ���״̬ʱ������Χ��3�����ϵĴ��ϸ��ʱ����ϸ���������״̬\n";
-            cout << "��ǰϸ��Ϊ����״̬ʱ������Χ��3�����ϸ��ʱ����ϸ����ɴ��״̬\n";
+            cout << "细胞是否生存取决于周围8个槽中的细胞数量\n";
+            cout << "当前细胞为存活状态时，当周围低于2个（不包含2个）存活细胞时， 该细胞变成死亡状态\n";
+            cout << "当前细胞为存活状态时，当周围有2个或3个存活细胞时， 该细胞保持原样\n";
+            cout << "当前细胞为存活状态时，当周围有3个以上的存活细胞时，该细胞变成死亡状态\n";
+            cout << "当前细胞为死亡状态时，当周围有3个存活细胞时，该细胞变成存活状态\n";
             break;
         case 'z':
             break;
         case 'w':
-            cells[rand()%20][rand()%20] = true;
+            cells[rand() % 20][rand() % 20] = true;
             cout << "create complete\n";
             break;
         case 'e':
             system("cls");
             develop();
             system("cls");
-<<<<<<< current
-            cout << "��ֻ��5�����Ԥ��������֪�����������ֵ����VIP\n";
-            cout << "Thanks for playing this game, �d(������)Bye~Bye~\n";
-			getchar();
+            cout << "您只有5秒免费预览，若想知道后续，请充值心悦VIP\n";
+            cout << "Thanks for playing this game, ヾ(￣▽￣)Bye~Bye~\n";
+            getchar();
             return 0;
             break;
         default:
@@ -93,13 +90,13 @@ int main()
 void help()
 {
     cout << "_____________________________\n";
-    cout << " c: lay a cell\n";					//
-    cout << " q: preinstall some cells\n";		//
-    cout << " v: clear all the cells\n";		// 
-    cout << " b: view the cells\n";				//
-    cout << " s: rules of the cells\n";			//
-    cout << " z: help of this game\n";			//
-    cout << " w: create a cell randomly\n";		//
+    cout << " c: lay a cell\n"; //
+    cout << " q: preinstall some cells\n"; //
+    cout << " v: clear all the cells\n"; //
+    cout << " b: view the cells\n"; //
+    cout << " s: rules of the cells\n"; //
+    cout << " z: help of this game\n"; //
+    cout << " w: create a cell randomly\n"; //
     cout << " e: develop\n";
     cout << "_____________________________\n";
 }
@@ -112,7 +109,7 @@ void print()
     }
     cout << '\n';
     for (int i = 0; i < 20; ++i) {
-        cout << setw(2) << left << i+1 << ' ';
+        cout << setw(2) << left << i + 1 << ' ';
         for (int j = 0; j < 20; ++j)
             cout << setw(2) << static_cast<char>((cells[i][j]) ? 4 : ' ') << ' ';
         cout << '\n';
@@ -132,70 +129,62 @@ void develop()
             }
         memcpy(cells, temp, 400);
         print();
-        Sleep(1000/15);
+        Sleep(1000 / 15);
 
         // system("cls");
         HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
-        COORD pos { 0, 0 };
+        COORD pos{ 0, 0 };
         SetConsoleCursorPosition(handle, pos);
     }
 }
 
-bool change(int x, int y)	// the most important part
+bool change(int x, int y) // the most important part
 {
     int count = 0;
     // edge
-    if (x == 0 && y == 0) {	        	// left-up
+    if (x == 0 && y == 0) { // left-up
         for (int i = 0; i < 2; ++i)
             for (int j = 0; j < 2; ++j)
                 if (cells[i][j])
                     ++count;
-    }
-    else if (x == 0 && y == 19) {       // right-up
+    } else if (x == 0 && y == 19) { // right-up
         for (int i = 0; i < 2; ++i)
             for (int j = 18; j < 20; ++j)
                 if (cells[i][j])
                     ++count;
-    }
-    else if (x == 19 && y == 0) {    	// left-down
+    } else if (x == 19 && y == 0) { // left-down
         for (int i = 18; i < 20; ++i)
             for (int j = 0; j < 2; ++j)
                 if (cells[i][j])
                     ++count;
-    }
-    else if (x == 19 && y == 19) {      // right-down
+    } else if (x == 19 && y == 19) { // right-down
         for (int i = 18; i < 20; ++i)
             for (int j = 18; j < 20; ++j)
                 if (cells[i][j])
                     ++count;
-    }
-    else if (y == 0) {          		// left
-        for (int i = x-1; i < x+2; ++i)
+    } else if (y == 0) { // left
+        for (int i = x - 1; i < x + 2; ++i)
             for (int j = 0; j < 2; ++j)
                 if (cells[i][j])
                     ++count;
-    }
-    else if (x == 0) {                  // up
+    } else if (x == 0) { // up
         for (int i = 0; i < 2; ++i)
-            for (int j = y-1; j < y+2; ++j)
+            for (int j = y - 1; j < y + 2; ++j)
                 if (cells[i][j])
                     ++count;
-    }
-    else if (x == 19) {                 // down
+    } else if (x == 19) { // down
         for (int i = 18; i < 20; ++i)
-            for (int j = y-1; j < y+2; ++j)
+            for (int j = y - 1; j < y + 2; ++j)
                 if (cells[i][j])
                     ++count;
-    }
-    else if (y == 19) {                 // right
-        for (int i = x-1; i < x+2; ++i)
+    } else if (y == 19) { // right
+        for (int i = x - 1; i < x + 2; ++i)
             for (int j = 18; j < 20; ++j)
                 if (cells[i][j])
                     ++count;
-    }
-    else {                              // others
-        for (int i = x-1; i < x+2; ++i)
-            for (int j = y-1; j < y+2; ++j)
+    } else { // others
+        for (int i = x - 1; i < x + 2; ++i)
+            for (int j = y - 1; j < y + 2; ++j)
                 if (cells[i][j])
                     ++count;
     }
@@ -206,15 +195,10 @@ bool change(int x, int y)	// the most important part
             return false;
         else
             return true;
-    }
-    else {
+    } else {
         if (count == 3)
             return true;
         else
             return false;
     }
 }
-=======
-            cout << "��ֻ��5�����Ԥ��������֪�����������ֵ����VIP\n";
-            cout << "Thanks for playing this game, 
->>>>>>> before discard
