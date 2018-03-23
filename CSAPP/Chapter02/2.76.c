aed0adc8cecdcd855e300ebc5107124c33bdbe17
LiuChenxuan <1412167533@qq.com>
2018-03-23 13:31:08 +0800

    Chapter02

---
+++ CSAPP/Chapter02/2.76.c
@@ -0,0 +1,41 @@
+/*
+	Date: 2018.3.22
+	Author: 刘臣轩
+*/
+
+#include <assert.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void* calloc(size_t nmemb, size_t size);
+
+int main()
+{
+    void* p = calloc(0x1234, sizeof(int));
+    assert(p != NULL);
+    free(p);
+
+    p = calloc(SIZE_MAX, 1);
+    assert(p == NULL);
+
+    p = calloc(SIZE_MAX, 2);
+    assert(p == NULL);
+    return 0;
+}
+
+void* calloc(size_t nmemb, size_t size)
+{
+    if (nmemb == 0 || size == 0)
+        return NULL;
+    size_t buf_size = nmemb * size;
+    if (nmemb && buf_size / nmemb == size) {
+        void* p = malloc(buf_size);
+        if (p == NULL)
+            return NULL;
+        memset(p, 0, buf_size);
+        return p;
+    }
+    return NULL;
+}
